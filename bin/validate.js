da46a7414f8c083cc4cc2232cb5cc0688e8276b7
James <james.speirs@gmail.com>
2017-03-21 09:55:20 -0600

    add composition and inheritance validation

--- bin/validate.js
+++ bin/validate.js
@@ -18,7 +18,7 @@
 const normalize         = require('./normalize');
 const is                = require('./is');
 
-exports.request = function(req, schema) {
+exports.request = function(req, schema, definitions) {
     const errors = [];
     const queryParams = {};
 
@@ -54,7 +54,7 @@ exports.request = function(req, schema) {
             if (store.hasOwnProperty(name)) {
                 const value = store[name];
                 const schema = param.in === 'body' ? param.schema : param;
-                const err = exports.byType(value, schema, 2);
+                const err = exports.byType(value, schema, definitions, 2);
                 if (err) errors.push('Error in ' + param.in + (param.in === 'body' ? '' : ' for parameter: ' + name) + '. ' + err.replace(/^\s+/, ''));
             }
         });
@@ -70,16 +70,7 @@ exports.request = function(req, schema) {
 };
 
 exports.response = function(value, schema, definitions, depth) {
-    let err;
-    if (Array.isArray(schema.allOf)) {
-        err = allOfValidation(value, schema.allOf, depth, Object.keys(value));
-    } else if (schema.discriminator) {
-        const allOf = [ schema ];
-        err = buildAllOfForDiscriminator(value, schema.discriminator, definitions, allOf);
-        if (!err) err = allOfValidation(value, allOf, depth, Object.keys(value));
-    } else {
-        err = exports.byType(value, schema, depth);
-    }
+    const err = exports.byType(value, schema, definitions, depth);
     if (err) return 'Response did not meet swagger requirements:\n' + err;
 };
 
@@ -88,7 +79,7 @@ exports.response = function(value, schema, definitions, depth) {
 
 
 
-exports.array = function(value, schema, depth) {
+exports.array = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (!is.array(value)) return error('Expected an array.');
 
@@ -102,7 +93,7 @@ exports.array = function(value, schema, depth) {
             if (uniqueMap && uniqueMap.has(item)) return error('Expected an array of unique items.', 'Value is not unique: ' + item);
             if (uniqueMap) uniqueMap.set(item, true);
             if (schema.items) {
-                const err = exports.byType(item, schema.items, (depth || 0) + 2);
+                const err = exports.byType(item, schema.items, definitions, (depth || 0) + 2);
                 if (err) return error.spaces + 'Error at index ' + index + ': ' + err.replace(/^\s+/, '');
             }
         })
@@ -111,42 +102,42 @@ exports.array = function(value, schema, depth) {
     return '';
 };
 
-exports.binary = function(value, schema, depth) {
+exports.binary = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (!normalize.rxBinary.test(value) || value.length % 8 !== 0) return error('Expected a binary sequence of octets.');
-    return generic(value, schema, depth);
+    return generic(value, schema, definitions, depth);
 };
 
-exports.byte = function(value, schema, depth) {
+exports.byte = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (!normalize.rxBase64.test(value)) return error('Expected a base64 encoded value.');
-    return generic(value, schema, depth);
+    return generic(value, schema, definitions, depth);
 };
 
-exports.byType = function(value, schema, depth) {
+exports.byType = function(value, schema, definitions, depth) {
     const type = normalize.schemaType(schema);
     switch (type) {
-        case 'array': return exports.array(value, schema, depth);
+        case 'array': return exports.array(value, schema, definitions, depth);
         case 'boolean': return;
         case 'file': return;
-        case 'integer': return exports.integer(value, schema, depth);
-        case 'number': return exports.number(value, schema, depth);
-        case 'object': return exports.object(value, schema, depth);
+        case 'integer': return exports.integer(value, schema, definitions, depth);
+        case 'number': return exports.number(value, schema, definitions, depth);
+        case 'object': return exports.object(value, schema, definitions, depth);
         case 'string':
-            if (!schema.hasOwnProperty('format')) return exports.string(value, schema, depth);
+            if (!schema.hasOwnProperty('format')) return exports.string(value, schema, definitions, depth);
             switch (schema.format) {
-                case 'binary': return exports.binary(value, schema, depth);
-                case 'byte': return exports.byte(value, schema, depth);
-                case 'date': return exports.date(value, schema, depth);
-                case 'date-time': return exports.dateTime(value, schema, depth);
-                default: return exports.string(value, schema, depth);
+                case 'binary': return exports.binary(value, schema, definitions, depth);
+                case 'byte': return exports.byte(value, schema, definitions, depth);
+                case 'date': return exports.date(value, schema, definitions, depth);
+                case 'date-time': return exports.dateTime(value, schema, definitions, depth);
+                default: return exports.string(value, schema, definitions, depth);
             }
             break;
     }
     return '';
 };
 
-exports.date = function(value, schema, depth) {
+exports.date = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (typeof value === 'string') {
         const match = normalize.rxDate.exec(value);
@@ -158,18 +149,18 @@ exports.date = function(value, schema, depth) {
         const date = new Date(year, month, day);
         if (date.getFullYear() !== year || date.getMonth() !== month || date.getDay() !== day) return error('Date does not exist on the calendar.');
 
-        return exports.dateTime(value + 'T00:00:00Z', schema, depth);
+        return exports.dateTime(value + 'T00:00:00Z', schema, definitions, depth);
 
     } else if (is.dateObject(value)) {
         const date = normalize.startOfDay(value);
-        return exports.dateTime(date, schema, depth);
+        return exports.dateTime(date, schema, definitions, depth);
 
     } else {
         return error('Expected a date.');
     }
 };
 
-exports.dateTime = function(value, schema, depth) {
+exports.dateTime = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     const isDate = is.dateObject(value);
 
@@ -193,16 +184,16 @@ exports.dateTime = function(value, schema, depth) {
     const numericSchema = Object.assign({}, schema);
     if (schema.hasOwnProperty('maximum')) numericSchema.maximum = +normalize.toDate(schema.maximum);
     if (schema.hasOwnProperty('minimum')) numericSchema.minimum = +normalize.toDate(schema.minimum);
-    return exports.number(numeric, numericSchema, depth);
+    return exports.number(numeric, numericSchema, definitions, depth);
 };
 
-exports.integer = function(value, schema, depth) {
+exports.integer = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (!is.number(value) || !normalize.rxInteger.test(value)) return error('Expected an integer.');
-    return exports.number(value, schema, depth);
+    return exports.number(value, schema, definitions, depth);
 };
 
-exports.number = function(value, schema, depth) {
+exports.number = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (!is.number(value) || !normalize.rxNumber.test(value)) return error('Expected a number.');
 
@@ -212,43 +203,23 @@ exports.number = function(value, schema, depth) {
     if (schema.hasOwnProperty('minimum') && value < schema.minimum) return error('Expected the number to be greater than or equal to ' + schema.minimum + '.');
     if (schema.hasOwnProperty('minimum') && schema.exclusiveMinimum && value <= schema.minimum) return error('Expected the number to be greater than ' + schema.maximum + '.');
 
-    return generic(value, schema, depth);
+    return generic(value, schema, definitions, depth);
 };
 
-exports.object = function(value, schema, depth) {
+exports.object = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
-    if (!is.nonNullObject(value)) return error('Expected a non-null object.');
+    const isObject = typeof value === 'object';
 
-    const keys = Object.keys(value);
-    const length = keys.length;
-    if (schema.hasOwnProperty('maxProperties') && length > schema.maxProperties) return error('Expected an object with a maximum of ' + schema.maxProperties + '.', 'Received: ' + keys.join(', ') + ' (' + length + ')');
-    if (schema.hasOwnProperty('minProperties') && length < schema.minProperties) return error('Expected an object with a minimum of ' + schema.minProperties + '.', 'Received: ' + keys.join(', ') + ' (' + length + ')');
-
-    const hasDefinedProperties = schema.hasOwnProperty('properties');
-    const requiredProperties = hasDefinedProperties
-        ? Object.keys(schema.properties)
-            .filter(key => schema.properties[key].required)
-            .reduce((p, c) => { p[c] = true; return p; }, {})
-        : {};
-
-    const errors = hasDefinedProperties
-        ? keys.map(key => {
-                if (!schema.properties.hasOwnProperty(key)) return error('Property not allowed: ', key);
-                delete requiredProperties[key];
-                const err = exports.byType(value[key], schema.properties[key], depth + 1);
-                if (err) return error.spaces + '  Error in property "' + key + '": ' + err.replace(/^\s+/, '');
-            })
-            .filter(v => v)
-        : [];
-    if (errors.length > 0) return error.spaces + 'Object had one or more errors: \n' + errors.join('\n');
-
-    const missingRequired = Object.keys(requiredProperties);
-    if (missingRequired.length > 0) return error.spaces + 'Object missing required properties: ' + missingRequired.join(', ');
-
-    return generic(value, schema, depth);
+    // validate inheritances
+    if (value && isObject && (schema.allOf || schema.discriminator)) {
+        const errors = validateInheritances(value, schema, definitions, depth);
+        if (errors.length > 0) return error.spaces + 'Object had one or more errors: \n' + errors.join('\n' + error.spaces + '  ');
+    } else if (isObject) {
+        return plainObject(value, schema, definitions, depth);
+    }
 };
 
-exports.string = function(value, schema, depth) {
+exports.string = function(value, schema, definitions, depth) {
     const error = errorGen(value, depth);
     if (typeof value !== 'string') return error('Expected a string.');
 
@@ -259,56 +230,16 @@ exports.string = function(value, schema, depth) {
         if (!rx.test(value)) return error('Expected string to match pattern ' + schema.pattern + '.');
     }
 
-    return generic(value, schema, depth);
+    return generic(value, schema, definitions, depth);
 };
 
 
-
-// validate multiple schemas
-function allOfValidation(value, allOf, depth, notValidated) {
-    const error = errorGen(value, depth);
-    if (!value || typeof value !== 'object') return error('Expected a non-null object to exercise allOf validation.');
-
-    const length = allOf.length;
-    for (let i = 0; i < length; i++) {
-        const schema = allOf[i];
-        if (Array.isArray(schema.allOf)) {
-            return allOfValidation(value, schema.allOf, depth, notValidated);
-        } else {
-            const properties = schema.properties && typeof schema.properties === 'object' ? Object.keys(schema.properties) : [];
-            const partial = value ? normalize.partialObject(value, properties) : value;
-            const err = exports.byType(partial, schema, depth);
-            if (err) return err;
-
-            // remove any properties that have now been validated at least once
-            notValidated = notValidated.filter(name => properties.indexOf(name) === -1);
-        }
-    }
-
-    if (notValidated.length > 0) return error('Object has one or more properties that are not allowed: ' + notValidated.join(', ') + '.', ' ');
-}
-
-function buildAllOfForDiscriminator(value, discriminator, definitions, allOf) {
-    if (value && typeof value === 'object' && value.hasOwnProperty(discriminator)) {
-        const name = value[discriminator];
-        if (definitions.hasOwnProperty(name)) {
-            const definition = definitions[name];
-
-            // prevent duplicate allOfs - no need to validate same thing twice
-            if (Array.isArray(definition.allOf)) {
-                definition.allOf.forEach(d => allOf.indexOf(d) === -1 ? allOf.push(d) : null);
-            } else if (allOf.indexOf(definition) === -1) {
-                allOf.push(definition);
-            }
-            if (definition.hasOwnProperty('discriminator')) return buildAllOfForDiscriminator(value, definition.discriminator, definitions, allOf);
-        } else {
-            return 'Could not find definition "' + name + '" for discriminator "' + discriminator;
-        }
-    } else {
-        return 'Expected the value to be a non-null object with property ' + discriminator + ' to exercise discriminator validation.';
-    }
-}
-
+/**
+ * Produce an error generator function.
+ * @param {*} value
+ * @param {number} depth
+ * @returns {Function}
+ */
 function errorGen(value, depth) {
     if (!depth) depth = 0;
     let spaces = '';
@@ -323,8 +254,125 @@ function errorGen(value, depth) {
     return fn;
 }
 
-function generic(value, schema, prefix) {
-    const error = errorGen(value, prefix);
-    if (schema.hasOwnProperty('enum') && schema.enum.indexOf(value) === -1) return error('Expected one of: ' + schema.enum.join(', ') + '.');
-    return '';
+/**
+ * Run generic validation that is applicable to all types.
+ * @param {*} value
+ * @param {object} schema
+ * @param {object} definitions
+ * @param {string} prefix
+ * @returns {*}
+ */
+function generic(value, schema, definitions, prefix) {
+    return schema.hasOwnProperty('enum') && schema.enum.indexOf(value) === -1
+        ? errorGen(value, prefix)('Expected one of: ' + schema.enum.join(', ') + '.')
+        : '';
+}
+
+/**
+ * Validate an object (ignoring inheritances)
+ * @param {*} value
+ * @param {object} schema
+ * @param {object} definitions
+ * @param {number} depth
+ * @returns {*}
+ */
+function plainObject(value, schema, definitions, depth) {
+    const error = errorGen(value, depth);
+    const keys = Object.keys(value);
+
+    if (!is.nonNullObject(value)) return error('Expected a non-null object.');
+
+    if (schema.hasOwnProperty('maxProperties') && length > schema.maxProperties) return error('Expected an object with a maximum of ' + schema.maxProperties + '.', 'Received: ' + keys.join(', ') + ' (' + length + ')');
+    if (schema.hasOwnProperty('minProperties') && length < schema.minProperties) return error('Expected an object with a minimum of ' + schema.minProperties + '.', 'Received: ' + keys.join(', ') + ' (' + length + ')');
+
+    const hasDefinedProperties = schema.hasOwnProperty('properties');
+    const requiredProperties = hasDefinedProperties
+        ? Object.keys(schema.properties)
+            .filter(key => schema.properties[key].required)
+            .reduce((p, c) => { p[c] = true; return p; }, {})
+        : {};
+
+    const errors = hasDefinedProperties
+        ? keys.map(key => {
+            if (!schema.properties.hasOwnProperty(key)) return error('Property not allowed: ', key);
+            delete requiredProperties[key];
+            const err = exports.byType(value[key], schema.properties[key], definitions, depth + 1);
+            if (err) return error.spaces + '  Error in property "' + key + '": ' + err.replace(/^\s+/, '');
+        })
+            .filter(v => v)
+        : [];
+    if (errors.length > 0) return error.spaces + 'Object had one or more errors: \n' + errors.join('\n');
+
+    const missingRequired = Object.keys(requiredProperties);
+    if (missingRequired.length > 0) return error.spaces + 'Object missing required properties: ' + missingRequired.join(', ');
+
+    return generic(value, schema, definitions, depth);
+}
+
+/**
+ * Validate an object with composition or inheritance.
+ * @param {*} value
+ * @param {object} schema
+ * @param {object} definitions
+ * @param {number} depth
+ * @returns {*}
+ */
+function validateInheritances(value, schema, definitions, depth) {
+    const buildErrors = [];
+    const map = new Map();
+    const schemas = [];
+
+    function build(schema) {
+        if (!schema || typeof schema !== 'object' || map.has(schema)) return;
+
+        const hasAllOf = Array.isArray(schema.allOf);
+        const notAllOf = !hasAllOf;
+        map.set(schema, notAllOf);
+        if (notAllOf) schemas.push(schema);
+
+        if (hasAllOf) {
+            schema.allOf.forEach(build);
+
+        } else if (schema.hasOwnProperty('discriminator')) {
+            const discriminator = schema.discriminator;
+            const name = value[discriminator];
+
+            if (!value.hasOwnProperty(discriminator)) {
+                buildErrors.push('Missing required discriminator property: ' + discriminator);
+            } else if (!definitions.hasOwnProperty(name)) {
+                buildErrors.push('Could not find definition "' + name + '" for discriminator: ' + discriminator);
+            } else {
+                const defSchema = definitions[name];
+                build(defSchema);
+            }
+        }
+    }
+
+    build(schema);
+    if (buildErrors.length > 0) return buildErrors;
+
+    const properties = Object.keys(value);
+    const unused = properties.slice(0);
+    const errors = schemas
+        .map(schema => {
+            const schemaProperties = Object.keys(schema.properties);
+            const partial = value ? normalize.partialObject(value, schemaProperties) : value;
+            const error = plainObject(partial, schema, definitions, depth);
+            if (error) return error;
+
+            if (unused.length > 0) {
+                Object.keys(partial)
+                    .forEach(key => {
+                        const index = unused.indexOf(key);
+                        if (index !== -1) unused.splice(index, 1);
+                    });
+            }
+        })
+        .filter(v => !!v);
+
+    if (unused.length > 0) {
+        errors.push('One or more properties not allowed: ' + unused.join(', '));
+    }
+
+    return errors;
 }
